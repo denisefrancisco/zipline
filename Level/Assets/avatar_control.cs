a77bdc37c884940878cbb907c51deac9513fde30
Denise Francisco <cfrancisco@wesleyan.edu>
2016-03-08 09:14:13 -0800

    adding animation for avatar draft

---
+++ Level/Assets/avatar_control.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class avatar_control : MonoBehaviour {
+	//declaring variable for animation body
+	private Animator anim;
+
+	// Use this for initialization
+	void Start () {
+		//initializing animation instance of avatar
+		anim = gameObject.GetComponent<Animator> ();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		//updating speed characteristic
+		anim.speed = 0.5f;
+	}
+}
