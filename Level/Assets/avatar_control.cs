25398f74d49e63164d3ad3c6ba90642c31ba75fe
Denise Francisco <cfrancisco@wesleyan.edu>
2016-03-08 12:31:19 -0800

    character control
    character can be only controlled with left and right arrow. If the
    character moves off of the ground, the character will fall.

--- Level/Assets/avatar_control.cs
+++ Level/Assets/avatar_control.cs
@@ -4,16 +4,32 @@ using System.Collections;
 public class avatar_control : MonoBehaviour {
 	//declaring variable for animation body
 	private Animator anim;
+	public float speed;
+	private float movement_sp = 0.1f;
+	float someScale;
 
 	// Use this for initialization
 	void Start () {
 		//initializing animation instance of avatar
 		anim = gameObject.GetComponent<Animator> ();
+		someScale = transform.localScale.x;
 	}
-	
+
 	// Update is called once per frame
 	void Update () {
 		//updating speed characteristic
-		anim.speed = 0.5f;
+		anim.speed = 1f * speed;
+		var v = Input.GetAxis ("Horizontal");
+		Debug.Log ("Horizontal is");
+		Debug.Log (v);
+		//character movement across horizontal plane
+		if (Input.GetKey(KeyCode.RightArrow)) {
+			transform.localScale = new Vector2(someScale, transform.localScale.y);
+			transform.position += Vector3.right * movement_sp;
+		} else if (Input.GetKey (KeyCode.LeftArrow)) {
+			transform.localScale = new Vector2(-someScale, transform.localScale.y);
+			transform.position += Vector3.left * movement_sp;
+		}
 	}
+		
 }
