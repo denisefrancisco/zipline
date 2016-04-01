8c965667d0b1284a4447d683dc4a1caa1ac09957
Lili Borland <liliborland@gmail.com>
2016-03-31 23:38:14 -0400

    Deleted Debug.Log lines and cleaned up code

--- Level/Assets/Scripts/Line/snap_point.cs
+++ Level/Assets/Scripts/Line/snap_point.cs
@@ -14,41 +14,37 @@ public class snap_point : MonoBehaviour {
 	public bool validLineEndPoint;
 	public bool canDraw; //State of player's ability to draw lines
 
-	void OnMouseDown() { 
+	void OnMouseDown () { 
 		/* When a user mouses down on a snap point, that point becomes
 		 * a valid starting point for a new zip line segment but an
 		 * invalid ending point for that line segment  */
 		validLineStartPoint = true;
 		validLineEndPoint = false;
-		Debug.Log ("valid start pt & invalid end pt OnMouseDown");
 
 		//User can draw lines from this snap point
 		canDraw = true;
 		zipline.GetComponent<DrawPhysicsLine>().enabled = true;
-		Debug.Log ("enableDrawing true, DrawPhysics line enabled");
 	}
 
-	void OnMouseUp() {
+	void OnMouseUp () {
 		/* When a user mouses up on a snap point, that point becomes
 		 * a valid ending point for a zip line segment but an
 		 * invalid starting point for that line segment
 		 * (unless user mouses down on that same snap point) */		
 		validLineStartPoint = false;
 		validLineEndPoint = true;
-		Debug.Log ("valid end pt & invalid start pt OnMouseUp");
 
 		//User can't draw lines from this snap point if mouse is released
 		canDraw = false;
-		Debug.Log ("enableDrawing false OnMouseUp");
 	}
 
-	void OnMouseEnter() {
+	void OnMouseEnter () {
 		startcolor = renderer.color;	//Save initial color of snap point
 		//Change snap point color to black when mouse hovers over it
 		renderer.color = Color.black;	
 	}
 
-	void OnMouseExit() {
+	void OnMouseExit () {
 		//Change snap point color back to initial color when mouse exits it
 		renderer.color = startcolor;
 
@@ -56,7 +52,6 @@ public class snap_point : MonoBehaviour {
 		  disable DrawPhysicsLine script */
 		if (canDraw == false) {
 			zipline.GetComponent<DrawPhysicsLine>().enabled = false;
-			Debug.Log ("DrawPhysicsLine disabled OnMouseExit");
 		}
 	}
 
@@ -67,7 +62,6 @@ public class snap_point : MonoBehaviour {
 		zipline.GetComponent<DrawPhysicsLine>().enabled = false;
 		validLineStartPoint = false;
 		validLineEndPoint = false;
-		Debug.Log ("initialized snap point script");
 	}
 	
 	// Update is called once per frame
