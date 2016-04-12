2f67ec4d56ed7da02c1f0e339946a06453194a0b
Elli Scharlin <Elli@Ellis-MacBook-Air.local>
2016-04-12 13:56:56 -0400

    Added Script
    I began writing an animation zip for the button click. I added
    transitions and parameters, have not yet implemented them.

---
+++ Level/Assets/animOnButton.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class animOnButton : MonoBehaviour {
+	public Animator anim;
+
+	// Use this for initialization
+	void Start () {
+		anim.GetComponent<Animation>;
+	}
+	void OnTriggerEnter2D (Collider2D other) {
+		anim.SetTrigger ("onZip");
+	
+	}
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
