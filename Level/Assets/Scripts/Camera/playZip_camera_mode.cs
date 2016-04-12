6722557678de44aedd3de62e8b6654dc8fdea0e0
Denise Francisco <cfrancisco@wesleyan.edu>
2016-04-12 18:57:22 -0400

    Implementing UI menus and camera angle draft

---
+++ Level/Assets/Scripts/Camera/playZip_camera_mode.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class playZip_camera_mode : MonoBehaviour {
+
+	public GameObject player;
+	public Camera allcameraData;
+	private Vector3 offset = new Vector3 (0,12,-40);
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		Transform characterTransform = player.transform;
+		Vector3 characterPosition = characterTransform.position;
+		allcameraData.transform.position = characterPosition + offset;
+	}
+}
---
+++ Level/Assets/Scripts/Camera/start_camera.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class start_camera : MonoBehaviour {
+
+	public Camera camera;
+
+	// Use this for initialization
+	void Start () {
+		camera.orthographicSize = 500;
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
