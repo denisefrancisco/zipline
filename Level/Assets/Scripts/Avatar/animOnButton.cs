0fa2d0bd358c69de889c9e60c4ee21364677a48f
Elli Scharlin <Elli@Ellis-MacBook-Air.local>
2016-04-12 16:10:11 -0400

    Script Updates
    Added a button to the Scene. Updated the script and the avatar’s
    components.

--- Level/Assets/Scripts/Avatar/animOnButton.cs
+++ Level/Assets/Scripts/Avatar/animOnButton.cs
@@ -2,6 +2,7 @@
 using System.Collections;
 
 public class animOnButton : MonoBehaviour {
+	public GameObject avatar;
 	public Animator anim;
 
 	// Use this for initialization
@@ -12,8 +13,19 @@ public class animOnButton : MonoBehaviour {
 		*it is used to discern when the player is on the zip line
 		*and will trigger the animation to become avatar holding trolley.
 		*/
+//	void OnTriggerEnter2D (Collider2D other) {
+//		anim.SetTrigger ("onZip");
+//	
+//	}
+	void OnClick() {
+		Rigidbody2D rigid = avatar.AddComponent<Rigidbody2D> ();
+		rigid.mass = 4.0f;
+		rigid.angularDrag = 1.0f;
+		rigid.gravityScale = 1.0f;
+	}
 	void OnTriggerEnter2D (Collider2D other) {
-		anim.SetTrigger ("onZip");
+	
+		anim.SetTrigger ("onClick");
 	
 	}
 	// Update is called once per frame
