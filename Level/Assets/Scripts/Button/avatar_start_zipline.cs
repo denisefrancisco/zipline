0ffc97b5c6737f095184b47c15afe32a1fa2b74b
LiliBorland <LiliBorlandMacBook@LiliBorlandsAir.wireless.wesleyan.edu>
2016-04-12 14:55:45 -0400

    Added avatar_restart_zipline.cs script for restart button
    New avatar_restart_zipline.cs script destroys avatar’s rigid body (so
    avatar cannot interact with physics during build mode) and resets
    avatar to original start position and rotation (this sets the avatar up
    to traverse the zip line course again during play mode).

--- Level/Assets/Scripts/Button/avatar_start_zipline.cs
+++ Level/Assets/Scripts/Button/avatar_start_zipline.cs
@@ -5,10 +5,9 @@ public class avatar_start_zipline : MonoBehaviour {
 
 	public GameObject boyZipping;
 	private Rigidbody2D rigid;
-//	public void start_zip();
 
 	void OnClick() {
-		Rigidbody2D rigid = boyZipping.AddComponent<Rigidbody2D> ();
+		rigid = boyZipping.AddComponent<Rigidbody2D> ();
 		rigid.mass = 4.0f;
 		rigid.angularDrag = 1.0f;
 		rigid.gravityScale = 1.0f;
