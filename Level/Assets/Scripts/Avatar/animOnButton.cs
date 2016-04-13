47d855c575548cb29a34d7e0f2b5dbfd4b7c53fd
Elli Scharlin <Elli@Ellis-MacBook-Air.local>
2016-04-12 21:51:40 -0400

    Testing Physics
    Attempting to make player move slowly. This past commit is prior to
    downloading an asset.

--- Level/Assets/Scripts/Avatar/animOnButton.cs
+++ Level/Assets/Scripts/Avatar/animOnButton.cs
@@ -2,47 +2,63 @@
 using System.Collections;
 
 public class animOnButton : MonoBehaviour {
+
+
+	/*Variable 'go' will be used later as a force added to the rb to propel 
+			*him in x position animation.
+		*The 'movement_sp' float is used as a reference to speed. If the animation
+			*is moving too slowly increase this variable.
+		*The first Collider named 'feet' is the name of the box collider that 
+			*must be on the avatar.
+		*The next Collider named origin is the collider that the avatar
+			*begins on before zip lining.
+		*/
+
 	public GameObject avatar;
 	public Animator anim;
+
+
+
 	private bool go = false;
-	private float movement_sp = 2.2f;
+	private float movement_sp = 2.05f;
 	private Collider2D feet;
 	public Collider2D origin;
 	private Rigidbody2D rb;
-	// Use this for initialization
 
+// Use this for initialization
 	void Start () {
 		anim = GetComponent<Animator> ();
 		feet = GetComponent<BoxCollider2D> ();
 		rb = GetComponent<Rigidbody2D> ();
 	}
-	/*This trigger is a parameter in the animator.
-		*it is used to discern when the player is on the zip line
-		*and will trigger the animation to become avatar holding trolley.
-		*/
-//	void OnTriggerEnter2D (Collider2D other) {
-//		anim.SetTrigger ("onZip");
-//	
-//	
+
+	//Use this for what happens when the button is clicked.
 	public void OnClick() {
-		Debug.Log ("hello");
+
+		//There is a trigger called "onClick" in the animator's parameters. 
 		anim.SetTrigger ("onClick");
 		go = true;
 	}
+	//The avatar progresses to jump animation.
 	void OnTriggerEnter2D (Collider2D other) {
 	
 		anim.SetTrigger ("onZip");
 	
 	}
-	// Update is called once per frame
+	//Update is called once per frame.
 	void Update () {
 		if (go) {
-			if (feet.IsTouching(origin)){ 				
-				rb.AddForce (Vector3.right * movement_sp, ForceMode2D.Impulse); // += Vector3.right * movement_sp;
-			}
 
+		/* The 'if' statement checks to see if the player is currently on the piece of furniture.
+			*  if he is, he will keep moving forward, if not he will recieve a force to
+			*  push him on the zip line initially.
+			*/
+
+
+			if (feet.IsTouching (origin)) { 				
+				rb.AddForce (Vector3.right * movement_sp, ForceMode2D.Impulse);
+			}
+		}
 	}
 }
-}
-	//anim.Play(anim.clip.name);
 
