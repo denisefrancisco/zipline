286a85c637eb857d37178ec89f5585812d229227
LiliBorland <LiliBorlandMacBook@LiliBorlandsAir.wireless.wesleyan.edu>
2016-04-17 09:20:54 -0400

    Added a FloorZone GO with box collider as trigger
    Indicates whether avatar has entered or exited the floor zone i.e. zone
    below the set of snap points. Avatar fails to traverse zipline course
    successfully if it enters the floor zone (must’ve fallen from the
    zipline course).

---
+++ Level/Assets/floorZone.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class floorZone : MonoBehaviour {
+
+	/* Trigger flag for whether avatar has fallen below
+	 * lowest snap point into the floor zone
+	 * (in which case the avatar has failed to traverse
+	 * the zipline course successfully) */
+	public bool inFloorZone; 
+
+	void OnTriggerEnter2D(Collider2D other) {
+		Debug.Log ("in floor zone");
+		inFloorZone = true;
+	}
+
+	void OnTriggerExit2D(Collider2D other) {
+		inFloorZone = false;
+	}
+
+	// Use this for initialization
+	void Start () {
+		inFloorZone = false;
+	}
+
+}
