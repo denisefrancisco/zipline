8ef2be0aca6a947da659fa18b69851f8d07b1e33
Denise Francisco <cfrancisco@wesleyan.edu>
2016-04-19 14:03:34 -0400

    cleaned up reset button on play mode

--- Level/Assets/Scripts/Camera/start_camera.cs
+++ Level/Assets/Scripts/Camera/start_camera.cs
@@ -5,10 +5,15 @@ public class start_camera : MonoBehaviour {
 
 	public Camera camera;
 
+	public void returnCamera(){
+		camera.orthographicSize = 5;
+		camera.transform.position = new Vector3 (-4.5f, camera.transform.position.y, camera.transform.position.z);
+	}
+
 	// Use this for initialization
 	void Start () {
 		camera.orthographicSize = 5;
-		camera.transform.position = new Vector3 (-5.0f, camera.transform.position.y, camera.transform.position.z);
+		camera.transform.position = new Vector3 (-4.5f, camera.transform.position.y, camera.transform.position.z);
 	
 	}
 	
