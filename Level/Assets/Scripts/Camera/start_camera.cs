872fe071b0717a1d5e6cfe7639b7d50af4999d6d
Denise Francisco <cfrancisco@wesleyan.edu>
2016-04-19 11:36:57 -0400

    initialized main camera to left side of screen

--- Level/Assets/Scripts/Camera/start_camera.cs
+++ Level/Assets/Scripts/Camera/start_camera.cs
@@ -8,6 +8,7 @@ public class start_camera : MonoBehaviour {
 	// Use this for initialization
 	void Start () {
 		camera.orthographicSize = 5;
+		camera.transform.position = new Vector3 (-5.0f, camera.transform.position.y, camera.transform.position.z);
 	
 	}
 	
