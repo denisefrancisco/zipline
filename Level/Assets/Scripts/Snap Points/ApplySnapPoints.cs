d0ea615d8ed9ee34165f6991e703e9891b6381fe
Denise Francisco <cfrancisco@wesleyan.edu>
2016-04-20 14:47:20 -0400

    ensured that all snap points were active after restarting level

---
+++ Level/Assets/Scripts/Snap Points/ApplySnapPoints.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ApplySnapPoints : MonoBehaviour {
+
+	public void applySnapPoint() {
+		foreach (Transform child in this.transform) {
+			child.gameObject.SetActive (true);
+		}
+	}
+
+	// Use this for initialization
+	void Start () {
+	}
+	// Update is called once per frame
+	void Update () {
+
+	}
+}
