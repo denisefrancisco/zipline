8561bcf883fbba08cd0e832ab7deee84801b523f
Anthony Muraco <amuraco@wesleyan.edu>
2016-04-20 01:30:23 -0400

    Implemented Timer
    Created a new scene in which I implemented a digital timer starting
    from 0 and counting upwards. (12.34)

---
+++ Level/Assets/Scripts/StopWatch/myTimer.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;// Very Important!
+
+public class myTimer : MonoBehaviour {
+
+	public float myCoolTimer = 0;
+	private Text timerText;
+
+	// Use this for initialization
+	void Start () {
+		timerText = GetComponent<Text> ();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		myCoolTimer += Time.deltaTime;
+		timerText.text = myCoolTimer.ToString ("f2");
+		print (myCoolTimer);
+	}
+}
