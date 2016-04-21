0a3c328911eea787e8230313762ddff278cd0c4c
Denise Francisco <cfrancisco@wesleyan.edu>
2016-04-21 05:46:30 -0400

    created play camera boundaries

---
+++ Level/Assets/Scripts/Camera/playZip_2.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class playZip_2 : MonoBehaviour {
+
+	public Transform player;
+	public Transform farLeft;
+	public Transform farRight;
+	public GameObject timer;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		Vector3 newPosition = transform.position;
+		newPosition.x = player.position.x;
+		newPosition.x = Mathf.Clamp (newPosition.x, farLeft.position.x, farRight.position.x);
+		newPosition.y = player.position.y;
+		transform.position = newPosition;
+		if (player.transform.position.x >= -3.2f) {
+			timer.SetActive (true);
+		}
+
+	}
+}
