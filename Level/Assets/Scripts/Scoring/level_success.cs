e0e9d306eacd8e48581650c6cc44e9815b6ecddd
Denise Francisco <cfrancisco@wesleyan.edu>
2016-04-21 20:51:54 -0400

    implemented finalized confetti

--- Level/Assets/Scripts/Scoring/level_success.cs
+++ Level/Assets/Scripts/Scoring/level_success.cs
@@ -8,11 +8,15 @@ public class level_success : MonoBehaviour {
 	public GameObject confetti;
 	private EdgeCollider2D ec;
 	private BoxCollider2D bc;
+	private Vector3 offset;
+	private Vector3 offset1;
 
 	// Use this for initialization
 	void Start () {
 		bc = gameObject.GetComponent<BoxCollider2D> ();
 		ec = avatar.GetComponent<EdgeCollider2D> ();
+		offset = new Vector3(-1,4,0);
+		offset1 = new Vector3(1,4,0);
 	}
 	
 	// Update is called once per frame
@@ -20,7 +24,8 @@ public class level_success : MonoBehaviour {
 		if (bc.IsTouching(ec)) {
 			Debug.Log ("YAYYYY");
 			success_modal.SetActive(true);
-			Instantiate (confetti, success_modal.transform.position, success_modal.transform.rotation);
+			Instantiate (confetti, success_modal.transform.position+offset, success_modal.transform.rotation);
+			Instantiate (confetti, success_modal.transform.position+offset1,success_modal.transform.rotation);
 			avatar.SetActive (false);
 		}
 	
