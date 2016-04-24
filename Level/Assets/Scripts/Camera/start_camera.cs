a25b0336bce242c11612a6a6e07a7429ea42f30e
Denise Francisco <cfrancisco@wesleyan.edu>
2016-04-23 21:03:03 -0400

    fixed camera to return to initial position after restarting a level

--- Level/Assets/Scripts/Camera/start_camera.cs
+++ Level/Assets/Scripts/Camera/start_camera.cs
@@ -4,17 +4,20 @@ using System.Collections;
 public class start_camera : MonoBehaviour {
 
 	public Camera camera;
+	//this variable is to indicate where the camera initializes to when the game is started
+	private Vector3 resetCameraPosition;
 
 	public void returnCamera(){
 		camera.orthographicSize = 5.5f;
-		//camera.transform.position = new Vector3 (-4.5f, camera.transform.position.y, camera.transform.position.z);
+		//return the camera to the top of the screen after restarting level
+		camera.transform.position = resetCameraPosition;
 	}
 
 	// Use this for initialization
 	void Start () {
-		Debug.Log (Time.timeScale);
+		//saving the starting position for the camera 
+		resetCameraPosition = gameObject.transform.position;
 		camera.orthographicSize = 5.5f;
-		//camera.transform.position = new Vector3 (-4.5f, camera.transform.position.y, camera.transform.position.z);
 	
 	}
 	
