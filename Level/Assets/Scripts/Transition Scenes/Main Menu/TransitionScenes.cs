f46d286339d3089e5ac71726cd9b2f5efa1aa6bc
LiliBorland <LiliBorlandMacBook@LiliBorlandsAir.wireless.wesleyan.edu>
2016-04-24 23:28:37 -0400

    Created TransitionsScenes.cs script with all public functions that load various scenes

---
+++ Level/Assets/Scripts/Transition Scenes/Main Menu/TransitionScenes.cs
@@ -0,0 +1,46 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.SceneManagement;
+
+// Script of public functions that load various scenes
+public class LoadScene : MonoBehaviour {
+
+	// Load main menu scene
+	public void MainMenu() {
+		SceneManager.LoadScene ("start_menu");
+	}
+
+	// Load select menu scene
+	public void SelectMenu() {
+		SceneManager.LoadScene ("select_menu");
+	}
+
+	// TODO: load help scene
+
+	// Load options menu scene
+	public void OptionsMenu() {
+		SceneManager.LoadScene ("options_menu");
+	}
+
+	// Load first avatar customization scene
+	public void ChooseGender() {
+		SceneManager.LoadScene ("zipline_choose_gender");
+	}
+
+	// Load level select (map) scene
+	public void MapMenu() {
+		SceneManager.LoadScene ("map_level");
+	}
+
+	// Load living room scene
+	public void LivingRoom() {
+		SceneManager.LoadScene ("Level1AnthonyScene");
+	}
+
+	// TODO: load lab scene
+
+	// Quit game
+	public void quitGame() {
+		Application.Quit();
+	}
+}
