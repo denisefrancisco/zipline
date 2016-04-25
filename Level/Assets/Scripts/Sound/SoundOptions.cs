f2a7175c2eb4d72cda7f71ebf60be518014aca57
LiliBorland <LiliBorlandMacBook@LiliBorlandsAir.wireless.wesleyan.edu>
2016-04-24 23:58:36 -0400

    Added SoundsOptions.cs script: A script to turn on/off ALL SOUND (will edit later when we implement music). Attached script to new ButtonScripts game object so we can call functions in SoundsOptions.cs script in options menu sound effects button

---
+++ Level/Assets/Scripts/Sound/SoundOptions.cs
@@ -0,0 +1,34 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class SoundOptions : MonoBehaviour {
+
+	// Reference to last sound volume level before turned off
+	private float sVolume;
+
+	public void SoundOn() {
+		AudioListener.volume = sVolume;
+		Debug.Log ("turned on sound to " + sVolume);
+	}
+
+	public void SoundOff() {
+		// Save current sound volume level before turning sound off
+		sVolume = AudioListener.volume;
+		AudioListener.volume = 0.0f;
+		Debug.Log ("turned off sound");
+	}
+
+	// TODO: Implement Music controls once we implement music
+	/*public void MusicOn() {
+	}
+	public void MusicOff() {
+	}*/
+
+	// Use this for initialization
+	void Start () {
+		// Initially sound is on
+		sVolume = AudioListener.volume;
+		Debug.Log ("current volume" + sVolume);
+	}
+
+}
