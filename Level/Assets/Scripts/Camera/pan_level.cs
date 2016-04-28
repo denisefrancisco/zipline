a85c80f128721348d28993a3be5239c859bfca81
Denise Francisco <cfrancisco@wesleyan.edu>
2016-04-28 18:10:39 -0400

    fixed panning level, but then i still need to integrate the way I pause and concentrate on the success door

---
+++ Level/Assets/Scripts/Camera/pan_level.cs
@@ -0,0 +1,41 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class pan_level : MonoBehaviour {
+
+	//panning the level, we need to use the upper and lower boundaries of the main camera.
+	public Transform upperBoundary;
+	public Transform lowerBoundary;
+	private Vector3 current_pos;
+	private pan_level script;
+	private Vector3 initial_pos;
+	private bool isDown = false;
+	public GameObject canvas;
+
+	// Use this for initialization
+	void Start () {
+//		GetComponent<Camera>().transform.position = lowerBoundary.position - new Vector3(0,0,0.2f);
+		initial_pos = gameObject.transform.position;
+		script = gameObject.GetComponent<pan_level>();
+		canvas.SetActive (false);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (GetComponent<Camera> ().transform.position.y - 0.2f >= lowerBoundary.position.y && isDown == false) {
+			current_pos = GetComponent<Camera> ().transform.position;
+			current_pos.y -= 0.05f;
+			GetComponent<Camera> ().transform.position = current_pos;
+		} else if (GetComponent<Camera> ().transform.position.y + 0.2f <= upperBoundary.position.y) {
+			isDown = true;
+			current_pos = GetComponent<Camera> ().transform.position;
+			current_pos.y += 0.05f;
+			GetComponent<Camera> ().transform.position = current_pos;
+		} else {
+			gameObject.transform.position = initial_pos;
+			canvas.SetActive (true);
+			script.enabled = false;
+		}
+	
+	}
+}
--- Level/Assets/Scripts/Camera/start_camera.cs
+++ Level/Assets/Scripts/Camera/start_camera.cs
@@ -3,10 +3,7 @@ using System.Collections;
 
 public class start_camera : MonoBehaviour {
 
-	//panning the level, we need to use the upper and lower boundaries of the main camera.
-	public Transform upperBoundary;
-	public Transform lowerBoundary;
-	private Vector3 current_pos;
+
 	//the list of gameobjects below represent the different combinations of outfits that are available to the player.
 	public GameObject brownBoy;
 	public GameObject blueBoy;
@@ -41,13 +38,11 @@ public class start_camera : MonoBehaviour {
 
 	}
 
-
 	//use this before initialization
 	void Awake() {
 		//saving the starting position for the camera 
 		resetCameraPosition = gameObject.transform.position;
 		camera.orthographicSize = 5.5f;
-//		camera.transform.position = lowerBoundary.position - new Vector3(0,0,0.2f);
 		//find the game object with the saved data
 		saved_data = GameObject.Find("Saved Data");
 		big_boy = GameObject.Find ("Boy");
@@ -62,27 +57,30 @@ public class start_camera : MonoBehaviour {
 		} else {
 			big_boy.transform.position = new Vector3 (0, 0, 1000);
 		}
-		clothing = saved_data.GetComponent<SelectedPlayer>();
-		if (clothing.chosen_outfit == "blue") {
-			blueBoy.SetActive (true);
-			avatar = blueBoy;
-		} else if (clothing.chosen_outfit == "lightBlue") {
-			lightBlueBoy.SetActive (true);
-			avatar = lightBlueBoy;
-		} else if (clothing.chosen_outfit == "girl_pink") {
-			girl_pink.SetActive (true);
-			avatar = girl_pink;
-		} else if (clothing.chosen_outfit == "girl_white") {
-			girl_white.SetActive (true);
-			avatar = girl_white;
-		} else if (clothing.chosen_outfit == "girl_blue") {
-			girl_blue.SetActive (true);
-			avatar = girl_blue;
-		} else if (clothing.chosen_outfit == "brown") {
-			brownBoy.SetActive (true);
-			avatar = brownBoy;
+		if (clothing != null) {
+			clothing = saved_data.GetComponent<SelectedPlayer> ();
+			if (clothing.chosen_outfit == "blue") {
+				blueBoy.SetActive (true);
+				avatar = blueBoy;
+			} else if (clothing.chosen_outfit == "lightBlue") {
+				lightBlueBoy.SetActive (true);
+				avatar = lightBlueBoy;
+			} else if (clothing.chosen_outfit == "girl_pink") {
+				girl_pink.SetActive (true);
+				avatar = girl_pink;
+			} else if (clothing.chosen_outfit == "girl_white") {
+				girl_white.SetActive (true);
+				avatar = girl_white;
+			} else if (clothing.chosen_outfit == "girl_blue") {
+				girl_blue.SetActive (true);
+				avatar = girl_blue;
+			} else if (clothing.chosen_outfit == "brown") {
+				brownBoy.SetActive (true);
+				avatar = brownBoy;
+			}
 		}
 		Debug.Log (avatar.name);
+
 	}
 	// Use this for initialization
 	void Start () {
@@ -91,16 +89,11 @@ public class start_camera : MonoBehaviour {
 
 	}
 
-	void panLevel(){
-		while (camera.transform.position.y != upperBoundary.position.y + 0.2f) {
-			current_pos = camera.transform.position;
-			current_pos.y += 0.1f;
-			camera.transform.position = current_pos;
-		}
-	}
+
 	
 	// Update is called once per frame
 	void Update () {
+
 	
 	}
 }
