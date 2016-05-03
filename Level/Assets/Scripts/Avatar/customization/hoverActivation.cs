11b41a25161c4dd026fb133c0fab0fff0275e413
Denise Francisco <cfrancisco@wesleyan.edu>
2016-05-02 20:31:46 -0400

    created empty game object that sets hover to false when it is an ipad build

---
+++ Level/Assets/Scripts/Avatar/customization/hoverActivation.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class hoverActivation : MonoBehaviour {
+
+	private GameObject girl;
+	private GameObject boy;
+
+	// Use this for initialization
+	void Start () {
+		girl = GameObject.Find("female");
+		boy = GameObject.Find("male");
+		if(SystemInfo.deviceModel.Contains("iPad")){
+			girl.GetComponent<Animator>().enabled = false;
+			boy.GetComponent<Animator>().enabled = false;
+		}
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
