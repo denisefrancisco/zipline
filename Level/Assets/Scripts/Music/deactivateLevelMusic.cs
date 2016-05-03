abd04c2a672c0562a8b452ab641b284b34d99b94
Denise Francisco <cfrancisco@wesleyan.edu>
2016-05-03 03:19:59 -0400

    reimplemented deactivatedLevelMusic.cs

--- Level/Assets/Scripts/Music/deactivateLevelMusic.cs
+++ Level/Assets/Scripts/Music/deactivateLevelMusic.cs
@@ -3,23 +3,21 @@ using System.Collections;
 
 public class deactivateLevelMusic : MonoBehaviour {
 
-	public GameObject success_modal;
-	public GameObject failure_modal;
 	private GameObject avatar;
 
 	// Use this for initialization
 	void Start () {
 		avatar = GameObject.FindGameObjectWithTag ("Avatar");
 	}
-	
+
 	// Update is called once per frame
 	void Update () {
-//		if (success_modal.activeSelf == true || failure_modal.activeSelf == true) {
+		//		if (success_modal.activeSelf == true || failure_modal.activeSelf == true) {
 		if (avatar.activeSelf == false){
 			gameObject.GetComponent<AudioSource> ().enabled = false;
 		} else {
 			gameObject.GetComponent<AudioSource> ().enabled = true;
 		}
-	
+
 	}
 }
