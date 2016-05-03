3cb0696c5078247b72b81acdb19ae1b5ce5ab5ee
Denise Francisco <cfrancisco@wesleyan.edu>
2016-05-03 05:36:58 -0400

    successfully fixed the loading page bug and customization

--- Level/Assets/Scripts/Transition Scenes/save_scene_number.cs
+++ Level/Assets/Scripts/Transition Scenes/save_scene_number.cs
@@ -6,11 +6,20 @@ public class save_scene_number : MonoBehaviour {
 	//this int is for the loading level, to decide which level to loading after the loading page
 	//***IMPORTANT**, when you want to call a level, please start indexing AT ZERO!!! (0)
 	public string[] levelList;
-	public int levelNum;
 	public string level;
 
 	// Use this for initialization
 	void Start () {
+		foreach (string x in levelList) {
+			Debug.Log (x);
+		}
+		if (GameObject.FindGameObjectsWithTag ("Level").Length > 1) {
+			foreach (GameObject x in GameObject.FindGameObjectsWithTag("Level")) {
+				if (x.transform.position.z == 500) {
+					Destroy (x);
+				}
+			}
+		}
 		DontDestroyOnLoad (gameObject);
 		levelList = new string[6];
 		levelList [0] = "LabLevel1";
@@ -23,6 +32,8 @@ public class save_scene_number : MonoBehaviour {
 
 	public void LoadLevelNum(int num) {
 		level = levelList [num];
+		Debug.Log ("THIS IS THE LEVEL");
+		Debug.Log (level);
 	}
 	
 	// Update is called once per frame
